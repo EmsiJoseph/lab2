a3833da770f6b6326dd3d1cf6a9a8415c5b9951e
Mc Joseph Agbanlog <mcagbanlog@student.apc.edu.ph>
2023-01-16 23:21:38 +0800

    Lesson 6 Strings

--- week5/script.js
+++ week5/script.js
@@ -7,3 +7,10 @@ let yearLevel = 2;
 let numberOfYearsInCollege = 4;
 
 let yearsLeftInCollege = numberOfYearsInCollege - yearLevel;
+
+let seniorHighSchool = "South East Asia Institute of Trade and Technology";
+
+let course = "Bachelor of Science in Information Technology";
+
+let major = "Mobile and Internet Technology";
+
