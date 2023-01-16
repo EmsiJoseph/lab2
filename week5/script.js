99ef4e6b73b1bf80c869561f87ca2eb3309678b9
Mc Joseph Agbanlog <mcagbanlog@student.apc.edu.ph>
2023-01-16 23:20:03 +0800

    Lessons 4-5 Numbers, Operators, Assignment, Math

--- week5/script.js
+++ week5/script.js
@@ -2,3 +2,8 @@ let age = 20;
 const fullName = "Mc Joseph Agbanlog";
 
 let isFullScholar = true;
+
+let yearLevel = 2;
+let numberOfYearsInCollege = 4;
+
+let yearsLeftInCollege = numberOfYearsInCollege - yearLevel;
