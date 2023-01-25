a604ff1f1ace417c48782aa105ff9480b11ccd3f
Mc Joseph Agbanlog <mcagbanlog@student.apc.edu.ph>
2023-01-25 13:25:10 +0800

    Lesson 20 Classes

--- week5/script.js
+++ week5/script.js
@@ -48,15 +48,16 @@ if (isFullScholar) {
     document.getElementById("scholarship").innerHTML = "Not a full-scholar";
   };
 
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
+  var tablinks = document.getElementsByClassName("tab-links");
+  var tabcontents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname){
-  for(tablink of tablinks){
-    tablink.classList.remove("active-link");
-  }
-  for(tabcontent of tabcontents){
-    tabcontent.classList.remove("active-tab");
-  }
-}
-
+  function opentab(tabname){
+    for(tablink of tablinks){
+      tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+      tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+  } 
