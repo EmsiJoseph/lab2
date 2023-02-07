6413d1e21eaa167699733b4f09d51fa32968be5b
Mc Joseph Agbanlog <mcagbanlog@student.apc.edu.ph>
2023-02-07 16:11:39 +0800

    Added onlick reload in submit btn

--- week9/comments.php
+++ week9/comments.php
@@ -47,7 +47,7 @@
             <input type="email" name="email" placeholder="Your Email" required>
             <input type="website" name="website" placeholder="Your Website">
             <textarea name="comment" rows="6" placeholder="Your Message"></textarea>
-            <button type="submit" class="btn btn2">Submit</button>
+            <button type="submit" class="btn btn2" onlick="location.reload();">Submit</button>
             </form>
 
             <?php include 'form.php'; ?>
