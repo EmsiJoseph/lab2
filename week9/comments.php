507b387fdf3d88c93e5198a48b3f24970998629e
Mc Joseph Agbanlog <mcagbanlog@student.apc.edu.ph>
2023-02-07 16:44:06 +0800

    Updated comments.php

--- week9/comments.php
+++ week9/comments.php
@@ -1,84 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <!-- -----------meta--------- -->
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+    <!-- -----------meta--------- -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- ---------links-------- -->
-  <!-- -------- CSS ---------- -->
-  <link rel="stylesheet" href="comments.css" \>
-  <link rel="stylesheet" href="index.css" \>
-  
-  <script src="https://kit.fontawesome.com/d12174c34c.js" crossorigin="anonymous"></script>
-  <title>Comments</title>
+
+    <!-- ---------links-------- -->
+    <!-- -------- CSS ---------- -->
+    <link rel="stylesheet" href="comments.css" \>
+    <link rel="stylesheet" href="index.css" \>
+
+    <script src="https://kit.fontawesome.com/d12174c34c.js" crossorigin="anonymous"></script>
+    <title>Comments</title>
 </head>
+
 <body>
-<div class="header">
-    <h1 class="container">
-      <nav>
-        <a href="index.php">
-          <img src="images/icon.png" class="logo">
-        </a>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Portfolio</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </nav>  
-</div>
+    <div class="header">
+        <h1 class="container">
+            <nav>
+                <a href="index.php">
+                    <img src="images/icon.png" class="logo">
+                </a>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+    </div>
 
-<div id="comment">
-    <div class="container">
-      <div class="row">
-        <div class="comment-left">
-          <hi class="sub-title">Comments</hi>
-          <br><br>
-          <?php include 'getComments.php'; ?>
+    <div id="comment">
+        <div class="container">
+            <div class="row">
+                <div class="comment-left">
+                    <hi class="sub-title">Comments</hi>
+                    <br><br>
+                    <?php include 'getComments.php'; ?>
+                </div>
+                <div class="contact-right">
+                    <!-- --------Form------------ -->
+
+                    <form id="comment-form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <input type="text" name="name" placeholder="Your Name" required>
+                        <input type="email" name="email" placeholder="Your Email" required>
+                        <input type="website" name="website" placeholder="Your Website">
+                        <textarea name="comment" rows="6" placeholder="Your Message"></textarea>
+                        <button type="submit" class="btn btn2">Submit</button>
+                    </form>
+                    <?php include 'getComments.php'; ?>
+                    <?php include 'form.php'; ?>
+
+                </div>
+            </div>
         </div>
-        <div class="contact-right">
-          <!-- --------Form------------ -->
-        
-            <form id="comment-form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-            <input type="text" name="name" placeholder="Your Name" required>
-            <input type="email" name="email" placeholder="Your Email" required>
-            <input type="website" name="website" placeholder="Your Website">
-            <textarea name="comment" rows="6" placeholder="Your Message"></textarea>
-            <button type="submit" class="btn btn2">Submit</button>
-            </form>
-
-            <?php include 'form.php'; ?>
-  
-        </div>
-      </div>
-
-  <!-- ---------- AJAX for reloading content -------- -->
-
-  <script>
-              $(document).ready(function() {
-              $("form").submit(function(e) {
-              e.preventDefault();
-
-              // submit the form using AJAX
-              $.ajax({
-                type: "POST",
-                url: "/getComments.php",
-                data: $(this).serialize(),
-                success: function(data) {
-               // on success, update the comments section with the new data
-                $("#comments").html(data);
-                }
-             });
-            });
-          });
-          </script>
+    </div>
 
 
 
 
-  
+
+
 </body>
+
 </html>
